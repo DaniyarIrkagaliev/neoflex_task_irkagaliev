44c5b00e7508d92c37fe037f701c545de258b2ce
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-10-13 17:16:19 +0300

    styles for Home component moved to a separate file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import "../../App.scss"
 import Card from "./card/Card";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-
+import "./homeStyles.scss"
 const Home = () => {
 
     // localStorage.clear()
